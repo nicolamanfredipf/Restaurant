a52963e8c582bce5618014646a9455952e10f0ac
Nicola Manfredi <nicola.manfredi.210796@gmail.com>
2024-10-29 15:35:52 +0100

    fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heebo: ['Heebo', 'sans-serif'],
+        nunito: ['Nunito','sans-serif'],
+        pacifico: ["Pacifico", 'cursive']
+      },
+      colors: {
+        "accent-white": "F1F8FF",
+        "accent-orange": "#FEA116",
+        "accent-black": "#0F172B"
+      }
+    },
   },
   plugins: [],
 }
